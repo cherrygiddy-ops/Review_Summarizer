9d6a2f8afefffc1c9f2077b81d2c563986ba6714
Morris <morrischerryg@gmail.com>
2025-12-18 13:58:13 +0300

    refactor prompts

--- packages/server/service/review.service.ts
+++ packages/server/service/review.service.ts
@@ -2,6 +2,7 @@ import OpenAI from 'openai';
 import type { Review } from '../generated/prisma';
 import reviewRepository from '../repositories/review.repository';
 import { llmClient } from '../llm/client';
+import template from '../prompts/summarize.reviews.txt';
 
 export const reviewService = {
    async getReviews(productId: number): Promise<Review[]> {
@@ -11,7 +12,7 @@ export const reviewService = {
       const reviews = await reviewRepository.getReviews(productId, 10);
 
       const joinReviews = reviews.map((rev) => rev.content).join('\n\n');
-      const prompt = `Summarize the following customer reviews into a short paragraph highlighting key themes, both positive and negative: ${joinReviews}`;
+      const prompt = template.replace('{{reviews}}', joinReviews);
       const response = await llmClient.generateText({
          model: 'gpt-4.1',
          prompt,
