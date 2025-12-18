56c0221c26655a0d14d6af9de6a12d7f42b120bb
Morris <morrischerryg@gmail.com>
2025-12-18 16:10:29 +0300

    handle edge cases

--- packages/server/controllers/review.controller.ts
+++ packages/server/controllers/review.controller.ts
@@ -1,12 +1,16 @@
 import type { Request, Response } from 'express';
 import { reviewService } from '../service/review.service';
+import { productRepository } from '../repositories/product.repository';
+import reviewRepository from '../repositories/review.repository';
 
 export const reviewController = {
    async getReviews(req: Request, res: Response) {
       const productId = Number(req.params.id);
       if (isNaN(productId)) {
          res.status(400).json({ error: 'invalid product id passed' });
+         return;
       }
+
       const reviews = await reviewService.getReviews(productId);
       res.json(reviews);
    },
@@ -15,6 +19,16 @@ export const reviewController = {
       var productId = Number(req.params.id);
       if (isNaN(productId)) {
          res.status(400).json({ error: 'invalid product id' });
+      }
+      const product = await productRepository.getProduct(productId);
+      if (!product) {
+         res.status(400).json({ error: 'product doesnt exists' });
+         return;
+      }
+      const reviews = await reviewRepository.getReviews(productId, 1);
+      if (reviews.length < 1) {
+         res.status(400).json({ error: 'no reviews to sumarrieze' });
+         return;
       }
       const summary = await reviewService.summarizeReviews(productId);
 
--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -4,5 +4,8 @@ import { reviewController } from './controllers/review.controller';
 const router = express.Router();
 
 router.get('/api/products/:id/reviews', reviewController.getReviews);
-router.post('/api/products/:id/summarize', reviewController.summarizeReviews);
+router.post(
+   '/api/products/:id/reviews/summarize',
+   reviewController.summarizeReviews
+);
 export default router;
